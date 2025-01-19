e4a6fff77e5c9fa0102b47bea5051ed4fe413fa8
vipulm9003 <60426192+vipulm9003@users.noreply.github.com>
2025-01-20 01:33:29 +0530

    Add files via upload

---
+++ exam.java
@@ -0,0 +1,14 @@
+package org.sample;
+
+public class exam {
+	 public static void main(String[] args) {
+	       
+	        String friendName = "Rashi";
+
+	        
+	        System.out.println("Hi " + friendName + ",");
+	        System.out.println("Wishing you all the very best for your CDAC exam!");
+	        System.out.println("Ts tr YOU ARE BEST ahech");
+	        System.out.println("Best Luck Once Again!");
+	    }
+}
